22c8ec66ab3d3214642d003b24592c85778d6e51
Nicolas2595 <nmorelli10@gmail.com>
2021-04-28 17:34:18 +0200

    aggiunto innnerHTML

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -6,11 +6,13 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 // Generare un  numero random da 1 a 6 per il giocatore
 var numeroGiocatore = Math.floor(Math.random() * 6);
-console.log("Numero random giocatore " + numeroGiocatore);
+console.log("Numero random giocatore ", numeroGiocatore);
+document.getElementById("numero_giocatore").innerHTML = "Numero giocatore: " + numeroGiocatore;
 
 // Generare un numero random da 1 a 6 per il computer
 var numeroComputer = Math.floor(Math.random() * 6);
-console.log("Numero random computer " + numeroComputer);
+console.log("Numero random computer ", numeroComputer);
+document.getElementById("numero_computer").innerHTML = "Numero computer: " + numeroComputer;
 
 // Stabilire il vincitore
 if (numeroGiocatore > numeroComputer) {
