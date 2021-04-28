cb8b70f935e3eb7cf32b2ee86e5bff63b2c74376
Nicolas2595 <nmorelli10@gmail.com>
2021-04-28 16:59:37 +0200

    2 dadi

---
+++ dadi/js/script.js
@@ -0,0 +1,22 @@
+/*
+2 - Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
+
+// Generare un  numero random da 1 a 6 per il giocatore
+var numeroGiocatore = Math.floor(Math.random() * 6);
+console.log("Numero random giocatore " + numeroGiocatore);
+
+// Generare un numero random da 1 a 6 per il computer
+var numeroComputer = Math.floor(Math.random() * 6);
+console.log("Numero random computer " + numeroComputer);
+
+// Stabilire il vincitore
+if (numeroGiocatore > numeroComputer) {
+    alert("Ha vinto il giocatore!");
+} else if (numeroGiocatore == numeroComputer) {
+    alert("Avete pareggiato!");
+} else {
+    alert("Hai perso!");
+}
